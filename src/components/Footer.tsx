c20473ed77c2ef914a2dae0721138860e2bf35b0
Irwing <86335744+Irwingduran@users.noreply.github.com>
2023-10-14 23:12:56 -0600

    Add files via upload

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between">
+      <Link href="/" className="font-bold text-xl">Chiltepin</Link>
+      <p>© TODOS LOS DERECHOS RESERVADOS.</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/data.ts
@@ -0,0 +1,393 @@
+type Product = {
+  id: number;
+  title: string;
+  desc?: string;
+  img?: string;
+  price: number;
+  options?: { title: string; additionalPrice: number }[];
+};
+
+type Products = Product[];
+
+export const featuredProducts: Products = [
+  {
+    id: 1,
+    title: "Platillo",
+    desc: "descripcion descripcion descripcion descripcion",
+    img: "/temporary/p4.png",
+    price: 0.00,
+    options: [
+      {
+        title: "Chico",
+        additionalPrice: 0,
+      },
+      {
+        title: "Mediano",
+        additionalPrice: 0,
+      },
+      {
+        title: "Grande",
+        additionalPrice: 0,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Platillo",
+    desc: "descripcion descripcion descripcion descripcion",
+    img: "/temporary/p4.png",
+    price: 0.00,
+    options: [
+      {
+        title: "Small",
+        additionalPrice: 0,
+      },
+      {
+        title: "Medium",
+        additionalPrice: 4,
+      },
+      {
+        title: "Large",
+        additionalPrice: 6,
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Platillo",
+    desc: "descripcion descripcion descripcion descripcion",
+    img: "/temporary/p4.png",
+    price: 0.00,
+    options: [
+      {
+        title: "Small",
+        additionalPrice: 0,
+      },
+      {
+        title: "Medium",
+        additionalPrice: 4,
+      },
+      {
+        title: "Large",
+        additionalPrice: 6,
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Platillo",
+    desc: "descripcion descripcion descripcion descripcion",
+    img: "/temporary/p4.png",
+    price: 0.00,
+    options: [
+      {
+        title: "Small",
+        additionalPrice: 0,
+      },
+      {
+        title: "Medium",
+        additionalPrice: 4,
+      },
+      {
+        title: "Large",
+        additionalPrice: 6,
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Platillo",
+    desc: "descripcion descripcion descripcion descripcion",
+    img: "/temporary/p4.png",
+    price: 0.00,
+    options: [
+      {
+        title: "Small",
+        additionalPrice: 0,
+      },
+      {
+        title: "Medium",
+        additionalPrice: 4,
+      },
+      {
+        title: "Large",
+        additionalPrice: 6,
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "Platillo",
+    desc: "descripcion descripcion descripcion descripcion",
+    img: "/temporary/p4.png",
+    price: 0.00,
+    options: [
+      {
+        title: "Small",
+        additionalPrice: 0,
+      },
+      {
+        title: "Medium",
+        additionalPrice: 4,
+      },
+      {
+        title: "Large",
+        additionalPrice: 6,
+      },
+    ],
+  },
+  {
+    id: 7,
+    title: "Platillo",
+    desc: "descripcion descripcion descripcion descripcion",
+    img: "/temporary/p4.png",
+    price: 0.00,
+    options: [
+      {
+        title: "Small",
+        additionalPrice: 0,
+      },
+      {
+        title: "Medium",
+        additionalPrice: 4,
+      },
+      {
+        title: "Large",
+        additionalPrice: 6,
+      },
+    ],
+  },
+  {
+    id: 8,
+    title: "Platillo",
+    desc: "descripcion descripcion descripcion descripcion",
+    img: "/temporary/p4.png",
+    price: 0.00,
+    options: [
+      {
+        title: "Small",
+        additionalPrice: 0,
+      },
+      {
+        title: "Medium",
+        additionalPrice: 4,
+      },
+      {
+        title: "Large",
+        additionalPrice: 6,
+      },
+    ],
+  },
+  {
+    id: 9,
+    title: "Platillo",
+    desc: "descripcion descripcion descripcion descripcion",
+    img: "/temporary/p4.png",
+    price: 0.00,
+    options: [
+      {
+        title: "Small",
+        additionalPrice: 0,
+      },
+      {
+        title: "Medium",
+        additionalPrice: 4,
+      },
+      {
+        title: "Large",
+        additionalPrice: 6,
+      },
+    ],
+  },
+];
+
+
+
+
+export const pizzas: Products = [
+  {
+    id: 1,
+    title: "Platillo",
+    desc: "descripcion descripcion descripcion descripcion",
+    img: "/temporary/p4.png",
+    price: 0.00,
+    options: [
+      {
+        title: "Small",
+        additionalPrice: 0,
+      },
+      {
+        title: "Medium",
+        additionalPrice: 4,
+      },
+      {
+        title: "Large",
+        additionalPrice: 6,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Platillo",
+    desc: "descripcion descripcion descripcion descripcion",
+    img: "/temporary/p4.png",
+    price: 0.00,
+    options: [
+      {
+        title: "Small",
+        additionalPrice: 0,
+      },
+      {
+        title: "Medium",
+        additionalPrice: 4,
+      },
+      {
+        title: "Large",
+        additionalPrice: 6,
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Platillo",
+    desc: "descripcion descripcion descripcion descripcion",
+    img: "/temporary/p4.png",
+    price: 0.00,
+    options: [
+      {
+        title: "Small",
+        additionalPrice: 0,
+      },
+      {
+        title: "Medium",
+        additionalPrice: 4,
+      },
+      {
+        title: "Large",
+        additionalPrice: 6,
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Platillo",
+    desc: "descripcion descripcion descripcion descripcion",
+    img: "/temporary/p4.png",
+    price: 0.00,
+    options: [
+      {
+        title: "Small",
+        additionalPrice: 0,
+      },
+      {
+        title: "Medium",
+        additionalPrice: 4,
+      },
+      {
+        title: "Large",
+        additionalPrice: 6,
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Platillo",
+    desc: "descripcion descripcion descripcion descripcion",
+    img: "/temporary/p4.png",
+    price: 0.00,
+    options: [
+      {
+        title: "Small",
+        additionalPrice: 0,
+      },
+      {
+        title: "Medium",
+        additionalPrice: 4,
+      },
+      {
+        title: "Large",
+        additionalPrice: 6,
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "Platillo",
+    desc: "descripcion descripcion descripcion descripcion",
+    img: "/temporary/p4.png",
+    price: 0.00,
+    options: [
+      {
+        title: "Small",
+        additionalPrice: 0,
+      },
+      {
+        title: "Medium",
+        additionalPrice: 4,
+      },
+      {
+        title: "Large",
+        additionalPrice: 6,
+      },
+    ],
+  },
+];
+
+export const singleProduct: Product = {
+  id: 1,
+  title: "Platillo",
+  desc: "descripcion descripcion descripcion descripcion",
+  img: "/temporary/p4.png",
+  price: 0.00,
+  options: [
+    {
+      title: "Small",
+      additionalPrice: 0,
+    },
+    {
+      title: "Medium",
+      additionalPrice: 4,
+    },
+    {
+      title: "Large",
+      additionalPrice: 6,
+    },
+  ],
+};
+
+
+type Menu = {
+  id: number;
+  slug: string;
+  title: string;
+  desc?: string;
+  img?: string;
+  color: string;
+}[];
+
+export const menu: Menu = [
+  {
+    id: 1,
+    slug: "pastas",
+    title: "Variedad 1",
+    desc: "descripcion descripcion descripcion",
+    img: "/temporary/m1.png",
+    color: "white",
+  },
+  {
+    id: 2,
+    slug: "burgers",
+    title: "Variedad 2",
+    desc: "descripcion descripcion descripcion",
+    img: "/temporary/m2.png",
+    color: "black",
+  },
+  {
+    id: 3,
+    slug: "pizzas",
+    title: "Variedad 3",
+    desc: "Pizza Paradise: Irresistible slices, mouthwatering toppings, and cheesy perfection.",
+    img: "/temporary/m3.png",
+    color: "white",
+  },
+];
